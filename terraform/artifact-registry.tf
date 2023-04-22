69d5063439a938edc5f34679368b00a2e5ced1dc
sl2902 <laxmsun@hotmail.com>
2023-04-22 22:33:57 +0530

    Changed repository name

--- terraform/artifact-registry.tf
+++ terraform/artifact-registry.tf
@@ -1,5 +1,5 @@
 resource "google_artifact_registry_repository" "my-project" {
-  repository_id = "data-engineering-zoomcamp"
+  repository_id = "xetra"
   location      = var.GCP_REGION
   format        = "docker"
 }
