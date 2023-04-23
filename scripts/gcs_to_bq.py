6cde7ec1503c15b466254a5f3619d0bc8a00f7f6
sl2902 <laxmsun@hotmail.com>
2023-04-23 23:45:23 +0530

    Enhanced script to support loading data from file

--- scripts/gcs_to_bq.py
+++ scripts/gcs_to_bq.py
@@ -10,23 +10,36 @@ from prefect_gcp import GcpCredentials
 from google.cloud import storage
 from google.cloud import bigquery
 import google
-from typing import Iterator
+from typing import Iterator, Union
 import argparse
 from dateutil.parser import parse
 import pyarrow
 from pathlib import Path
+import json
+import csv
+from datetime import datetime
+from hashlib import sha1
+from calendar import monthrange
+import time
+
+def sha(row):
+    """Apply sha1() to the key"""
+    return sha1(row).hexdigest()
 
 @task
 def dbt_transform() -> None:
     """Run the dbt transformations on our BigQuery table"""
 
-    dbt_path = f"{os.getcwd()}/dbt/xetra"
+    DBT_PATH = os.environ.get("DBT_PATH")
+    DBT_HOME = os.environ.get("DBT_HOME")
+    
+    dbt_path = f"{os.getcwd()}/{DBT_PATH}"
 
     dbt_op = DbtCoreOperation(
         commands=["dbt build"],
         working_dir=dbt_path,
         project_dir=dbt_path,
-        profiles_dir=f"{Path.home()}/.dbt/",
+        profiles_dir=f"{Path.home()}/{DBT_HOME}/",
     )
 
     dbt_op.run()
@@ -43,8 +56,14 @@ def gcs_to_bq(file: str) -> None:
         # df["Date"] = pd.to_datetime(df["Date"].dt.date)
         # pyarrow.lib.ArrowTypeError: Expected bytes, got a 'datetime.time' object
         # df["Time"] = pd.to_timedelta(df["Time"].astype(str))
+        df["key"] = df["ISIN"].fillna("") + "_" + df["Date"].astype(str).fillna("") + "_" + df["Time"].astype(str).fillna("")
+        df["key"] = df["key"].str.lower().str.encode('utf-8').apply(sha)
         print(f"Number of rows {df.shape}")
         table_schema=[
+            {
+                "name": "key",
+                "type": "STRING",
+            },
             {
                 "name": "ISIN",
                 "type": "STRING",
@@ -159,6 +178,39 @@ def list_gcs(bucket_name: str, prefix: str, delimiter: str) -> Iterator[str]:
     for blob in blobs:
         yield blob.name
 
+@task(log_prints=True, name="dataset_load_check")
+def is_dataset_loaded(prefix: str) -> Union[dict, None]:
+    """Check whether dataset has been loaded or not"""
+    try:
+        ds = pd.read_csv("config/datasets_loaded.csv")
+    except Exception as e:
+        raise(e)
+    date = prefix.split("/")[-1]
+    record = ds.query("load_date == @date")
+    if record.shape[0] > 0:
+        return record.to_dict(orient="records")[0]
+
+
+@task(log_prints=True, name="history_load")
+def historical_load(filename: str) -> Iterator[str]:
+    """Read json file to load historical datasets"""
+    try:
+        with open(filename, "r") as f:
+            data = json.load(f)
+            for period in data["load_date"]:
+                yield period
+    except Exception as e:
+        raise(e)
+
+@task(log_prints=True, name="update_dataload")
+def update_datasets_loaded(filename: str, prefix: str):
+    """Update the datasets_loaded config file to keep 
+    track of the datasets loaded"""
+    with open(filename, 'a') as f:
+        writer = csv.writer(f)
+        rec = [prefix.split("/")[-1], datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")]
+        writer.writerow(rec)
+    
 @flow(log_prints=True, name="Pipeline to read files from GCS and load to BigQuery")
 def pipeline(bucket_name: str, prefix: str, delimiter: str = "") -> int:
     """Loop through files in GCS directory"""
@@ -171,17 +223,45 @@ def pipeline(bucket_name: str, prefix: str, delimiter: str = "") -> int:
     return num
 
 @flow(log_prints=True, name="Entry point")
-def main(prefix: str) -> None:
+def main(prefix: str, history_file: str) -> None:
     bucket_name = BUCKET_NAME
-    num = pipeline(bucket_name, prefix)
-    if num == 0:
-        print(f"No files for period {prefix}")
-    
-    if num > 0:
-        dbt_transform()
+    datasets_loaded_file = "config/datasets_loaded.csv"
+    if len(history_file) > 0:
+        for period in historical_load(history_file):
+            try:
+                yr, mn = datetime.strptime(period, "%Y-%m").strftime("%Y-%m").split("-")
+            except Exception as e:
+                raise(e)
+            for day in range(1, monthrange(int(yr), int(mn))[1] + 1):
+                dt = f"{yr}-{int(mn):02}-{day:02}"
+                prefix = f"{os.environ.get('GCP_PREFIX')}/{dt}"
+                rec = is_dataset_loaded(prefix)
+                if not rec is None:
+                    print(f"History load completed for {rec}")
+                    continue
+                num = pipeline(bucket_name, prefix)
+                if num == 0:
+                    print(f"No files for period {prefix.split('/')[-1]}")
+                
+                if num > 0:
+                    dbt_transform()
+                    update_datasets_loaded(datasets_loaded_file, prefix)
+    else:
+        rec = is_dataset_loaded(prefix)
+        if not rec is None:
+            print(f"History load completed for {rec}")
+        else:
+            num = pipeline(bucket_name, prefix)
+            if num == 0:
+                print(f"No files for period {prefix.split('/')[-1]}")
+            
+            if num > 0:
+                dbt_transform()
+                update_datasets_loaded(datasets_loaded_file, prefix)
         
 
 if __name__ == "__main__":
+    start = time.time()
     load_dotenv()
 
     DATASET_NAME = os.environ.get("GCP_DATASET_NAME")
@@ -192,10 +272,11 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser("Pipeline to read from GCS and write to BigQuery")
     parser.add_argument("--date", type=str, help="Enter period of load. Format yyyy-mm-dd", default="2022-03-25")
+    parser.add_argument("--file", type=str, help="Enter path to historical loading file.")
 
     args = parser.parse_args()
     if args.date:
-        prefix = args.date 
+        prefix = args.date
     
     if len(prefix) < 10:
         raise ValueError(f"Invalid date {prefix}. Format is yyyy-mm-dd")
@@ -206,4 +287,9 @@ if __name__ == "__main__":
     except ValueError:
         raise ValueError(f"Invalid date {prefix}. Format is yyyy-mm-dd")
     
-    main(f"{os.environ.get('GCP_PREFIX')}/{args.date}")
+    if args.file is None:
+        args.file = ""
+    
+    main(f"{os.environ.get('GCP_PREFIX')}/{args.date}", args.file)
+    end = time.time()
+    print(f"Load to BigQuery completed {(end-start)/60} mins")
