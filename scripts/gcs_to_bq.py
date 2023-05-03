91d5d59c3af666557a396d50c598194d761bf35d
sl2902 <laxmsun@hotmail.com>
2023-05-04 00:10:24 +0530

    Made changes to accommodate Prefect blocks

--- scripts/gcs_to_bq.py
+++ scripts/gcs_to_bq.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 from prefect import flow, task
 from prefect_dbt import DbtCoreOperation
 from prefect_gcp import GcpCredentials
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect.blocks.system import JSON
 from google.cloud import storage
 from google.cloud import bigquery
 import google
@@ -14,6 +16,7 @@ from typing import Iterator, Union
 import argparse
 from dateutil.parser import parse
 import pyarrow
+import pyarrow.parquet as pq
 from pathlib import Path
 import json
 import csv
@@ -21,38 +24,42 @@ from datetime import datetime
 from hashlib import sha1
 from calendar import monthrange
 import time
+import fsspec
+import google.auth
+
 
 PREFECT_GCP_CREDENTIALS_BLOCK_NAME = "de-prefect-gcpcreds"
+PREFECT_GCP_BUCKET = "de-prefect-gcpbucket"
 
 def sha(row):
     """Apply sha1() to the key"""
     return sha1(row).hexdigest()
 
-@task
-def gcp_credentials() -> None:
-    with open(SERVICE_ACCOUNT_FILE, "r") as file:
-        json_content = json.load(file)
+# @task
+# def gcp_credentials() -> None:
+#     with open(SERVICE_ACCOUNT_FILE, "r") as file:
+#         json_content = json.load(file)
 
-    service_account_info = {
-        "type": json_content["type"],
-        "project_id": json_content["project_id"],
-        "private_key_id": json_content["private_key_id"],
-        "private_key": json_content["private_key"],
-        "client_email": json_content["client_email"],
-        "client_id": json_content["client_id"],
-        "auth_uri": json_content["auth_uri"],
-        "token_uri": json_content["token_uri"],
-        "auth_provider_x509_cert_url": json_content["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": json_content["client_x509_cert_url"],
-    }
-    GcpCredentials(service_account_info=service_account_info).save("de-prefect-gcpcreds", overwrite=True)
+#     service_account_info = {
+#         "type": json_content["type"],
+#         "project_id": json_content["project_id"],
+#         "private_key_id": json_content["private_key_id"],
+#         "private_key": json_content["private_key"],
+#         "client_email": json_content["client_email"],
+#         "client_id": json_content["client_id"],
+#         "auth_uri": json_content["auth_uri"],
+#         "token_uri": json_content["token_uri"],
+#         "auth_provider_x509_cert_url": json_content["auth_provider_x509_cert_url"],
+#         "client_x509_cert_url": json_content["client_x509_cert_url"],
+#     }
+#     GcpCredentials(service_account_info=service_account_info).save("de-prefect-gcpcreds", overwrite=True)
 
 @task
-def dbt_transform() -> None:
+def dbt_transform(dbt_path: str, dbt_home: str) -> None:
     """Run the dbt transformations on our BigQuery table"""
 
-    DBT_PATH = os.environ.get("DBT_PATH")
-    DBT_HOME = os.environ.get("DBT_HOME")
+    DBT_PATH = dbt_path # os.environ.get("DBT_PATH")
+    DBT_HOME = dbt_home # os.environ.get("DBT_HOME")
 
     dbt_path = f"{os.getcwd()}/{DBT_PATH}"
 
@@ -66,106 +73,115 @@ def dbt_transform() -> None:
     dbt_op.run()
 
 @task
-def gcs_to_bq(file: str) -> None:
+def gcs_to_bq(file: str, 
+              credentials,
+              bucket_name: str, 
+              gcp_dataset_name: str, 
+              gcp_dataset_table: str) -> None:
     """Loading data to BigQuery"""
     # script runs but no data in BigQuery
-    gcp_credentials = GcpCredentials.load(
-        PREFECT_GCP_CREDENTIALS_BLOCK_NAME
-    )
+
+    # setting google client
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
     try:
-        df = pd.read_parquet(f"gs://{BUCKET_NAME}/{file}")
-        # df["Date"] = pd.to_datetime(df["Date"].dt.date)
-        # pyarrow.lib.ArrowTypeError: Expected bytes, got a 'datetime.time' object
-        # df["Time"] = pd.to_timedelta(df["Time"].astype(str))
-        df["key"] = df["ISIN"].fillna("") + "_" + df["Date"].astype(str).fillna("") + "_" + df["Time"].astype(str).fillna("")
-        df["key"] = df["key"].str.lower().str.encode('utf-8').apply(sha)
-        print(f"Number of rows {df.shape}")
-        table_schema=[
-            {
-                "name": "key",
-                "type": "STRING",
-            },
-            {
-                "name": "ISIN",
-                "type": "STRING",
-            },
-            {    
-                "name": "Mnemonic",
-                "type": "STRING",
-            },
-            {
-                "name": "SecurityDesc",
-                "type": "STRING",
-            },
-            {
-                "name": "SecurityType",
-                "type": "STRING",
-            },
-            {
-                "name": "Currency",
-                "type": "STRING",
-            },
-            {
-                "name": "SecurityID",
-                "type": "INT64",
-            },
-            {
-                "name": "Date",
-                "type": "TIMESTAMP",
-            },
-            {
-                "name": "Time",
-                "type": "STRING",
-            },
-            {
-                "name": "StartPrice",
-                "type": "FLOAT64",
-            },
-            {
-                "name": "MaxPrice",
-                "type": "FLOAT64",
-            },
-            {
-                "name": "MinPrice",
-                "type": "FLOAT64",
-            },
-            {
-                "name": "EndPrice",
-                "type": "FLOAT64",
-            },
-            {
-                "name": "TradedVolume",
-                "type": "INT64",
-            },
-            {
-                "name": "NumberOfTrades",
-                "type": "INT64",
-            }
-        ]
-        # pandas_gbq.to_gbq(
-        #     df,
-        #     destination_table=f"{DATASET_NAME}.{DATASET_TABLE}",
-        #     project_id=os.environ.get("GCP_PROJECT_ID"),
-        #     credentials=gcp_credentials.get_credentials_from_service_account(),
-        #     if_exists="append",
-        #     table_schema=table_schema
-        # )
-        # https://stackoverflow.com/questions/48886761/efficiently-write-a-pandas-dataframe-to-google-bigquery
-        client = bigquery.Client()
-        job_config = bigquery.LoadJobConfig()
-        job = client.load_table_from_dataframe(
-                    df, 
-                    f"{DATASET_NAME}.{DATASET_TABLE}", 
-                    job_config=job_config
-        )
-        job.result()
+        # df = pd.read_parquet(f"gs://{bucket_name}/{file}")
+        creds, _ =google.auth.default()
+        gc_fs = fsspec.filesystem("gcs", credentials=creds)
+        with gc_fs.open(f"gs://{bucket_name}/{file}") as f:
+            df = pq.read_table(f).to_pandas()
+
+            # df["Date"] = pd.to_datetime(df["Date"].dt.date)
+            # pyarrow.lib.ArrowTypeError: Expected bytes, got a 'datetime.time' object
+            # df["Time"] = pd.to_timedelta(df["Time"].astype(str))
+            df["key"] = df["ISIN"].fillna("") + "_" + df["Date"].astype(str).fillna("") + "_" + df["Time"].astype(str).fillna("")
+            df["key"] = df["key"].str.lower().str.encode('utf-8').apply(sha)
+            print(f"Number of rows {df.shape}")
+            table_schema=[
+                {
+                    "name": "key",
+                    "type": "STRING",
+                },
+                {
+                    "name": "ISIN",
+                    "type": "STRING",
+                },
+                {    
+                    "name": "Mnemonic",
+                    "type": "STRING",
+                },
+                {
+                    "name": "SecurityDesc",
+                    "type": "STRING",
+                },
+                {
+                    "name": "SecurityType",
+                    "type": "STRING",
+                },
+                {
+                    "name": "Currency",
+                    "type": "STRING",
+                },
+                {
+                    "name": "SecurityID",
+                    "type": "INT64",
+                },
+                {
+                    "name": "Date",
+                    "type": "TIMESTAMP",
+                },
+                {
+                    "name": "Time",
+                    "type": "STRING",
+                },
+                {
+                    "name": "StartPrice",
+                    "type": "FLOAT64",
+                },
+                {
+                    "name": "MaxPrice",
+                    "type": "FLOAT64",
+                },
+                {
+                    "name": "MinPrice",
+                    "type": "FLOAT64",
+                },
+                {
+                    "name": "EndPrice",
+                    "type": "FLOAT64",
+                },
+                {
+                    "name": "TradedVolume",
+                    "type": "INT64",
+                },
+                {
+                    "name": "NumberOfTrades",
+                    "type": "INT64",
+                }
+            ]
+            # pandas_gbq.to_gbq(
+            #     df,
+            #     destination_table=f"{DATASET_NAME}.{DATASET_TABLE}",
+            #     project_id=os.environ.get("GCP_PROJECT_ID"),
+            #     credentials=gcp_credentials.get_credentials_from_service_account(),
+            #     if_exists="append",
+            #     table_schema=table_schema
+            # )
+            # https://stackoverflow.com/questions/48886761/efficiently-write-a-pandas-dataframe-to-google-bigquery
+            # client = bigquery.Client()
+            job_config = bigquery.LoadJobConfig()
+            job = client.load_table_from_dataframe(
+                        df, 
+                        f"{gcp_dataset_name}.{gcp_dataset_table}", 
+                        job_config=job_config
+            )
+            job.result()
     except FileNotFoundError:
         pass
     except pyarrow.lib.ArrowTypeError:
         pass
 
 # @task
-# def gcs_to_bq(file: str) -> None:
+# def gcs_to_bq(file: str, bucket: str) -> None:
 #     """Loading data to BigQuery"""
 #     client = bigquery.Client()  
 #     job_config = bigquery.LoadJobConfig(
@@ -191,11 +207,15 @@ def gcs_to_bq(file: str) -> None:
 #     )
 
 @task(log_prints=True, name="list files from gcs")
-def list_gcs(bucket_name: str, prefix: str, delimiter: str) -> Iterator[str]:
+def list_gcs(credentials, bucket_name: str, prefix: str, delimiter: str) -> Iterator[str]:
     """Loop through files in GCS directory"""
-    storage_client = storage.Client()
+    # this works locally but not inside Docker; it was throwing an Anonymous caller error
+    # 401 no permission to Cloud storage
+    storage_client = storage.Client(credentials=credentials)
     # Note: Client.list_blobs requires at least package version 1.17.0.
     blobs = storage_client.list_blobs(bucket_name, prefix=prefix, delimiter=delimiter)
+    # storage = GcsBucket.load(PREFECT_GCP_BUCKET)
+    # blobs = storage.list_blobs(prefix)
     for blob in blobs:
         yield blob.name
 
@@ -233,22 +253,38 @@ def update_datasets_loaded(filename: str, prefix: str):
         writer.writerow(rec)
     
 @flow(log_prints=True, name="Pipeline to read files from GCS and load to BigQuery")
-def pipeline(bucket_name: str, prefix: str, delimiter: str = "") -> int:
+def pipeline(bucket_name: str, 
+             prefix: str, 
+             gcp_dataset_name: str, 
+             gcp_dataset_table: str, 
+             delimiter: str = "") -> int:
     """Loop through files in GCS directory"""
-    files = list_gcs(bucket_name, prefix, delimiter)
+    gcp_credentials = GcpCredentials.load(
+        PREFECT_GCP_CREDENTIALS_BLOCK_NAME
+    )
+    credentials = gcp_credentials.get_credentials_from_service_account()
+    files = list_gcs(credentials, bucket_name, prefix, delimiter)
     num = 0
     for file in files:
         print(f"file: {file}")
         num += 1
-        gcs_to_bq(file)
+        gcs_to_bq(file, credentials, bucket_name, gcp_dataset_name, gcp_dataset_table)
     return num
 
-@flow(log_prints=True, name="Entry point")
-def main(prefix: str, history_file: str) -> None:
-    gcp_credentials()
-    bucket_name = BUCKET_NAME
+@flow(log_prints=True)
+def main(prefix: str, history_file: str="") -> None:
+    # created a gcp credentials block
+    # gcp_credentials()
+    env = JSON.load("de-prefect-config")
+    bucket_name = env.value.get("GCP_BUCKET_NAME")
+    gcp_prefix = env.value.get("GCP_PREFIX")
+    gcp_dataset_name = env.value.get("GCP_DATASET_NAME")
+    gcp_dataset_table = env.value.get("GCP_DATASET_TABLE_NAME")
+    dbt_path = env.value.get("DBT_PATH")
+    dbt_home = env.value.get("DBT_HOME")
     datasets_loaded_file = "config/datasets_loaded.csv"
-    if len(history_file) > 0:
+    if len(history_file) > 0 and \
+    (history_file != "null" and history_file != "None" and history_file is not None):
         for period in historical_load(history_file):
             try:
                 yr, mn = datetime.strptime(period, "%Y-%m").strftime("%Y-%m").split("-")
@@ -256,47 +292,47 @@ def main(prefix: str, history_file: str) -> None:
                 raise(e)
             for day in range(1, monthrange(int(yr), int(mn))[1] + 1):
                 dt = f"{yr}-{int(mn):02}-{day:02}"
-                prefix = f"{os.environ.get('GCP_PREFIX')}/{dt}"
+                prefix = f"{gcp_prefix}/{dt}"
                 rec = is_dataset_loaded(prefix)
                 if not rec is None:
                     print(f"History load completed for {rec}")
                     continue
-                num = pipeline(bucket_name, prefix)
+                num = pipeline(bucket_name, prefix, gcp_dataset_name, gcp_dataset_table)
                 if num == 0:
                     print(f"No files for period {prefix.split('/')[-1]}")
                 
                 if num > 0:
-                    dbt_transform()
+                    dbt_transform(dbt_path, dbt_home)
                     update_datasets_loaded(datasets_loaded_file, prefix)
     else:
-        prefix = f"{os.environ.get('GCP_PREFIX')}/{prefix}"
+        prefix = f"{gcp_prefix}/{prefix}"
         rec = is_dataset_loaded(prefix)
         if not rec is None:
             print(f"History load completed for {rec}")
         else:
-            num = pipeline(bucket_name, prefix)
+            num = pipeline(bucket_name, prefix, gcp_dataset_name, gcp_dataset_table)
             if num == 0:
                 print(f"No files for period {prefix.split('/')[-1]}")
             
             if num > 0:
-                dbt_transform()
+                dbt_transform(dbt_path, dbt_home)
                 update_datasets_loaded(datasets_loaded_file, prefix)
         
 
 if __name__ == "__main__":
     start = time.time()
     # load_dotenv()
-
-    DATASET_NAME = os.environ.get("GCP_DATASET_NAME")
-    DATASET_TABLE = os.environ.get("GCP_DATASET_TABLE_NAME")
-    BUCKET_NAME = os.environ.get("GCP_BUCKET_NAME")
-    GCP_REGION = os.environ.get("GCP_REGION")
-    PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
-    SERVICE_ACCOUNT_FILE = os.environ.get("LOCAL_SERVICE_ACCOUNT_FILE_PATH")
+    # env = JSON.load("de-prefect-config")
+    # DATASET_NAME = env.value.get("GCP_DATASET_NAME")
+    # DATASET_TABLE = env.value.get("GCP_DATASET_TABLE_NAME")
+    # BUCKET_NAME = env.value.get("GCP_BUCKET_NAME")
+    # GCP_REGION = env.value.get("GCP_REGION")
+    # PROJECT_ID = env.value.get("GCP_PROJECT_ID")
+    # SERVICE_ACCOUNT_FILE = env.value.get("LOCAL_SERVICE_ACCOUNT_FILE_PATH")
 
     parser = argparse.ArgumentParser("Pipeline to read from GCS and write to BigQuery")
     parser.add_argument("--date", type=str, help="Enter period of load. Format yyyy-mm-dd", default="2022-04-22")
-    parser.add_argument("--file", type=str, help="Enter path to historical loading file.")
+    parser.add_argument("--file", type=str, help="Enter path to historical loading file.", default="")
 
     args = parser.parse_args()
     date_suffix = args.date
